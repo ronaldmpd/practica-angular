a301c7443a0ad99aac6bf4b75b1ca94278c8966a
DESKTOP-JMOH0LN\RomaN <rpinto@aitbol.com>
2017-11-17 16:58:57 -0400

    login complete

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { Router } from '@angular/router';
 import { UserManager } from '../shared/model/user-manager';
 import { UserService } from '../shared/services/user.service';
 
@@ -11,16 +11,29 @@ import { UserService } from '../shared/services/user.service';
 export class LoginComponent implements OnInit {
 
   userManager: UserManager;
+  isLoginError: boolean = false;
+  isDataLoading: boolean = false;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
     this.userManager = { id:0, username:'', password:'', fullName:''};    
   }
 
   onLogin(userManager: UserManager){
+    this.isDataLoading = true;
     this.userService.getLogin(userManager.username, userManager.password).subscribe(result =>{
       console.log('Login result', result);
+      this.isDataLoading = false;
+      if(result == undefined || result.length == 0)
+        this.isLoginError = true;
+      else
+        {
+          if(result.length == 1){
+            console.log("Login OK user: " + JSON.stringify(result[0]));
+            this.router.navigate(['/dashboard']);
+          }
+        }
     })
   }
 
